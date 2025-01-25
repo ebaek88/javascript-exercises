146324aac52a7a9be00fe0133b62d1ff9562f769
ebaek88 <93978710+ebaek88@users.noreply.github.com>
2025-01-24 20:05:55 -0700

    fix: add solutions upto 02_repeatString
    For 02_repeatString, the test.skip are modified to
    test in order to "unlock" all the test methods.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(stringToRepeat, numToRepeat) {
+  let result = "";
+  
+  if (numToRepeat < 0) {
+    return "ERROR";
+  }
 
+  for (let i = 0; i < numToRepeat; i++) {
+    result += stringToRepeat;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
