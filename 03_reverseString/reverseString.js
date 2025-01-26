c4d2bd795f62e5854a78980ce973e9227cb851f9
ebaek88 <93978710+ebaek88@users.noreply.github.com>
2025-01-25 17:34:17 -0700

    fix: add solutions upto 03_reverseString
    For 03_reverseString, the test.skip are modified to
    test in order to "unlock" all the test methods.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(str) {
+  let result = "";
 
+  for(let i = str.length - 1; i >= 0 ; i--) {
+    result += str[i];
+  }
+
+  return result;
+
+  // Also, this is possible
+  // return str.split("").reverse().join("");
+  // When the separator is "" in str.split(), the returned array will be an array of single characters
 };
 
 // Do not edit below this line
