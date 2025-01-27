1927b8bd5069cbe401e0be66c0a49ac3bec894b3
ebaek88 <93978710+ebaek88@users.noreply.github.com>
2025-01-27 11:12:41 -0700

    fix: add solutions upto 07_tempConversion
    For 07_tempConversion, the test.skip are modified to
    test in order to "unlock" all the test methods.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheitTemp) {
+  const convertedToCelsius = ((fahrenheitTemp - 32) * 5) / 9;
+  // using Math.round(num * 10) / 10 this time...
+  return Math.round(convertedToCelsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsiusTemp) {
+  const convertedToFahrenheit = (celsiusTemp * 9) / 5 + 32;
+  // using Number.parseFloat and Number.toFixed this time...
+  return Number.parseFloat(convertedToFahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
