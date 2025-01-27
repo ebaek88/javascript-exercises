ca5e2ebf356dc5eacd380d71059a6a1a5a3a3198
ebaek88 <93978710+ebaek88@users.noreply.github.com>
2025-01-26 17:40:39 -0700

    fix: add solutions upto 05_sumAll
    For 05_sumAll, the test.skip are modified to
    test in order to "unlock" all the test methods.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,36 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...query) {
+  const elementsToRemove = [];  // storing the elements of arr where arrayElem === elem
+  let result = [];  // storing the final result
+
+  // iterates over query
+  for (elem of query) {
+    // for every element of arr
+    for (arrayElem of arr) {
+      // if elem === elementToRemover, store the index in an array
+      if (arrayElem === elem) {
+        elementsToRemove.push(arrayElem);
+      }
+    }
+  }
+
+  // now iterate over the array of the elements to be removed that exist in the original array
+  for (elem of elementsToRemove) {
+    // toSpliced() does not modify the original array
+    // if we remove for the first time, use toSpliced() from the origianl array
+    if (result.length === 0) {
+      result = arr.toSpliced(arr.indexOf(elem), 1);
+    } else {  // if not, use toSpliced() from result
+      result = result.toSpliced(result.indexOf(elem), 1);
+    }
+  }
+
+  // if no removal is done, just return the original value
+  if (elementsToRemove.length === 0) {
+    return arr;
+  }
+
+  // else, return result
+  return result;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
+const sumAll = function(input1, input2) {
+  // checks if all the inputs are positive integers
+  if ( !( ( Number.isInteger(input1) && input1 > 0 ) && ( Number.isInteger(input2) && input2 > 0 ) ) ) {
+    return "ERROR";
+  }
 
+  return input1 >= input2 ? (input1 * (input1 + 1) / 2) - ((input2 - 1) * input2 / 2) : (input2 * (input2 + 1) / 2) - ((input1 - 1) * input1 / 2);
 };
 
 // Do not edit below this line
